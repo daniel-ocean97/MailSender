60b10cd56930eb5b6aac3d36266c4d6faedad787
Daniil Ermilov <yndx-daniel-ocean@yandex.ru>
2025-08-12 11:47:26 +0300

    Restrict mailing to own clients; filter form querysets; protect CRUD views and stats by owner

--- mail/forms.py
+++ mail/forms.py
@@ -22,3 +22,22 @@ class MailingForm(forms.ModelForm):
         widgets = {
             "clients": forms.CheckboxSelectMultiple,
         }
+    
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        
+        if user:
+            # Фильтруем клиентов только для текущего пользователя
+            if user.has_perm("mail.can_view_all_list"):
+                # Если у пользователя есть права на просмотр всех списков, показываем всех клиентов
+                self.fields['clients'].queryset = Client.objects.all()
+            else:
+                # Иначе показываем только клиентов текущего пользователя
+                self.fields['clients'].queryset = Client.objects.filter(owner=user)
+            
+            # Также фильтруем письма
+            if user.has_perm("mail.can_view_all_list"):
+                self.fields['message'].queryset = Letter.objects.all()
+            else:
+                self.fields['message'].queryset = Letter.objects.filter(owner=user)
--- mail/views.py
+++ mail/views.py
@@ -27,7 +27,7 @@ class ClientListView(LoginRequiredMixin, ListView):
         return queryset.filter(owner=self.request.user)
 
 
-class ClientCreateView(CreateView):
+class ClientCreateView(LoginRequiredMixin, CreateView):
     model = Client
     form_class = ClientForm
     template_name = "clients/client_form.html"
@@ -38,17 +38,29 @@ class ClientCreateView(CreateView):
         return super().form_valid(form)
 
 
-class ClientUpdateView(UpdateView):
+class ClientUpdateView(LoginRequiredMixin, UpdateView):
     model = Client
     form_class = ClientForm
     template_name = "clients/client_form.html"
     success_url = reverse_lazy("mail:client_list")
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.has_perm("mail.can_view_all_list"):
+            return queryset
+        return queryset.filter(owner=self.request.user)
 
-class ClientDeleteView(DeleteView):
+
+class ClientDeleteView(LoginRequiredMixin, DeleteView):
     model = Client
     template_name = "clients/client_confirm_delete.html"
     success_url = reverse_lazy("mail:client_list")
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.has_perm("mail.can_view_all_list"):
+            return queryset
+        return queryset.filter(owner=self.request.user)
 
 
 # Letter Views
@@ -68,7 +80,7 @@ class LetterListView(LoginRequiredMixin, ListView):
         return queryset.filter(owner=self.request.user)
 
 
-class LetterCreateView(CreateView):
+class LetterCreateView(LoginRequiredMixin, CreateView):
     model = Letter
     form_class = LetterForm
     template_name = "letters/letter_form.html"
@@ -79,17 +91,29 @@ class LetterCreateView(CreateView):
         return super().form_valid(form)
 
 
-class LetterUpdateView(UpdateView):
+class LetterUpdateView(LoginRequiredMixin, UpdateView):
     model = Letter
     form_class = LetterForm
     template_name = "letters/letter_form.html"
     success_url = reverse_lazy("mail:letter_list")
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.has_perm("mail.can_view_all_list"):
+            return queryset
+        return queryset.filter(owner=self.request.user)
 
-class LetterDeleteView(DeleteView):
+
+class LetterDeleteView(LoginRequiredMixin, DeleteView):
     model = Letter
     template_name = "letters/letter_confirm_delete.html"
     success_url = reverse_lazy("mail:letter_list")
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.has_perm("mail.can_view_all_list"):
+            return queryset
+        return queryset.filter(owner=self.request.user)
 
 
 # Mailing Views
@@ -109,55 +133,98 @@ class MailingListView(LoginRequiredMixin, ListView):
         return queryset.filter(owner=self.request.user)
 
 
-class MailingCreateView(CreateView):
+class MailingCreateView(LoginRequiredMixin, CreateView):
     model = Mailing
     form_class = MailingForm
     template_name = "mailing/mailing_form.html"
     success_url = reverse_lazy("mail:mailing_list")
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
 
 
-class MailingDetailView(DetailView):
+class MailingDetailView(LoginRequiredMixin, DetailView):
     model = Mailing
     template_name = "mailing/mailing_detail.html"
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.has_perm("mail.can_view_all_list"):
+            return queryset
+        return queryset.filter(owner=self.request.user)
 
-class MailingUpdateView(UpdateView):
+
+class MailingUpdateView(LoginRequiredMixin, UpdateView):
     model = Mailing
     form_class = MailingForm
     template_name = "mailing/mailing_form.html"
     success_url = reverse_lazy("mail:mailing_list")
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.has_perm("mail.can_view_all_list"):
+            return queryset
+        return queryset.filter(owner=self.request.user)
 
-class MailingDeleteView(DeleteView):
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+
+class MailingDeleteView(LoginRequiredMixin, DeleteView):
     model = Mailing
     template_name = "mailing/mailing_confirm_delete.html"
     success_url = reverse_lazy("mail:letter_list")
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.has_perm("mail.can_view_all_list"):
+            return queryset
+        return queryset.filter(owner=self.request.user)
+
 
 # Logging Views (только чтение)
-class LoggingListView(ListView):
+class LoggingListView(LoginRequiredMixin, ListView):
     model = Logging
     template_name = "logs/log_list.html"
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.has_perm("mail.can_view_all_list"):
+            return queryset
+        return queryset.filter(owner=self.request.user)
 
-class LoggingDetailView(DetailView):
+
+class LoggingDetailView(LoginRequiredMixin, DetailView):
     model = Logging
     template_name = "logs/log_detail.html"
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.has_perm("mail.can_view_all_list"):
+            return queryset
+        return queryset.filter(owner=self.request.user)
+
 
 def home(request):
-    # Общее количество рассылок
-    total_mailings = Mailing.objects.count()
-
-    # Количество активных рассылок (статус 'Запущена')
-    active_mailings = Mailing.objects.filter(status="started").count()
-
-    # Количество уникальных клиентов во всех рассылках
-    unique_clients = Client.objects.distinct().count()
+    # Фильтруем данные в зависимости от прав пользователя
+    if request.user.has_perm("mail.can_view_all_list"):
+        # Если у пользователя есть права на просмотр всех списков, показываем общую статистику
+        total_mailings = Mailing.objects.count()
+        active_mailings = Mailing.objects.filter(status="started").count()
+        unique_clients = Client.objects.distinct().count()
+    else:
+        # Иначе показываем только данные пользователя
+        total_mailings = Mailing.objects.filter(owner=request.user).count()
+        active_mailings = Mailing.objects.filter(owner=request.user, status="started").count()
+        unique_clients = Client.objects.filter(owner=request.user).distinct().count()
 
     context = {
         "total_mailings": total_mailings,
